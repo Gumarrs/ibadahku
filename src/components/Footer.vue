0c6b387cf534464a29d5c7d3dea8c8ba1435bdff
Gumarrs <agusmarsadu@gmail.com>
2025-07-05 15:36:17 +0700

    test

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="bg-gray-50 dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700 text-center py-4 text-sm text-gray-600 dark:text-gray-300 transition-colors">
     &copy; {{ year }} Copyright | 
-    <strong class="text-green-700 dark:text-green-400">Agus Marsadualan</strong>
+    <strong class="text-green-700 dark:text-green-400">Agus Marsadualan Ganteng</strong>
   </footer>
 </template>
 
